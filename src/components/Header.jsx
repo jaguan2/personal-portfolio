8e0cda2f6da010fd2f4fd08e1cde7437eb7ccf80
Jason Guan <126140314+jaguan2@users.noreply.github.com>
2026-01-04 20:11:20 -0500

    update nav bar color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
             <h1>Hi, I'm Jason 👋</h1>
             <p>"Passion makes the impossible happen"</p>
             <div className="home-button">
-              <a href={resumePdf} className="button button-primary" download="Jason_Guan_Resume.pdf">
+              <a href={resumePdf} className="button button-secondary" download="Jason_Guan_Resume.pdf">
                 Download CV
               </a>
               <a href="https://www.linkedin.com/in/jaguan/" className="button button-secondary" target="_blank" rel="noopener noreferrer">
